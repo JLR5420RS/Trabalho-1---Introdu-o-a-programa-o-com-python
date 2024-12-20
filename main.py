95bd52e1d3e699251046b7360a7a1e76acc84a79
Rommel <rommel.mod@gmail.com>
2024-12-20 01:31:16 -0300

    Divisão do código em funções como pedido inserido os arquivos do zip do trabalho e dividido o código inicial dentro do lib.py

--- main.py
+++ main.py
@@ -1,56 +1,26 @@
-#Trabalho 1 - X552 Introdução a programação com python
+from lib import download_dataset
+from lib import preencher_matriz_contratos
+from lib import imprimir_matriz
+from lib import exportar_csv
 
-# carrega o arquivo de dados
-resposta=(input("qual arquivo você deseja selecionar? digite 1 para entrada.txt ou digite 2 para entradagrande.txt: "))
-while resposta != "1" and resposta != "2":
-    resposta=(input("qual arquivo você deseja selecionar? digite 1 para entrada.txt ou digite 2 para entradagrande.txt: "))
-if resposta == "1":
-    arquivo=open("entrada.txt").readlines()
-else:
-    arquivo=open("entradagrande.txt").readlines()
-# armazena a linha sobre o pedido de contrato que a empresa quer
-pedido_empresa = arquivo[0].strip().split()
-# elimina essa linha do arquivo carregado
-arquivo.pop(0)
-# retira a quebra de linha (\n) entre as variáveis
-for linha in range(len(arquivo)):
-    arquivo[linha]=arquivo[linha].strip()
-# determinar o tamanho da matriz de valores de contrato
-n = int(pedido_empresa[1]) # fornecedor
-m = int(pedido_empresa[0]) # meses de contrato
-multa = float(pedido_empresa[2]) # multa de mudança de contrato
-# construir a matriz de contratos e incluir os valores infinitos
-contratos = []
-for fornecedor in range(n):
-    contratos.append([])
-    for inicio in range(m+1):
-        contratos[fornecedor].append([])
-        for fim in range(m+1):
-            contratos[fornecedor][inicio].append(float('inf'))
-# inserir os valores de contratos que estão no arquivo dentro de uma matriz
-for linha in arquivo:
-    dados = linha.split()
-    dados = [int(dados[0]), int(dados[1]), int(dados[2]), float(dados[3])]
-    fornecedor = (dados[0])-1
-    mes_inicial = dados[1]
-    mes_final = dados[2]
-    valor_contrato = dados[3]
-    contratos[fornecedor][mes_inicial][mes_final] = valor_contrato
-# salvar a matriz contratos dentro de um arquivo e mostrar o resultado no terminal    
-with open("contratos.txt", "w") as resultado:
-    for fornecedor in contratos:
-        print("\n")
-        resultado.write("\n")
-        for mes_inicial in fornecedor:
-            print(mes_inicial)
-            resultado.write(str(mes_inicial) + "\n")
-print("arquivo de resultado contratos.txt salvo")
+def main():
+    
+    # Download do dataset
+    # link_txt = "https://drive.google.com/file/d/1YjPaHv8aAVsXNzhHxum5gyUDFfY5iw1_/view?usp=drive_link"
+    link_txt = "https://drive.google.com/file/d/17KOe8bJvHDceTpGZ9ru1YvjlROIMWhZ3/view?usp=drive_link"
+    file_txt = 'dataset/entrada.txt'
+    download_dataset(link_txt,file_txt)
 
+    # Preencher a matriz de contratos
+    m, n, t, matriz = preencher_matriz_contratos(file_txt)
+    
+    # Imprimir os resultados
+    print(m, n, t, "\n")
+    imprimir_matriz(matriz)
 
-
-
-
-
-
-
-
+    # Exportar a matriz de contratos
+    file_csv = "resultados/contratos.csv"
+    exportar_csv(file_csv, matriz)
+    
+if __name__ == "__main__":
+    main()
